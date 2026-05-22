40663436d403b2af7b2e0747b2604ff47cb47971
Nobi <nobi@nobidev.com>
2026-05-22 10:37:25 +0700

    Rename Go package

--- lmtp_server_test.go
+++ lmtp_server_test.go
@@ -7,8 +7,6 @@ import (
 	"net"
 	"strings"
 	"testing"
-
-	"github.com/emersion/go-smtp"
 )
 
 func testServerGreetedLMTP(t *testing.T, fn ...serverConfigureFunc) (be *backend, s *smtp.Server, c net.Conn, scanner *bufio.Scanner) {
